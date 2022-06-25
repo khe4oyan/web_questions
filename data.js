c06247133042ac042a55cb0360bbd89d716d3208
khe4oyan <khe4oyan@bk.ru>
2022-06-25 23:51:01 +0400

    redesign arkhitecture

--- data.js
+++ data.js
@@ -1,39 +1,17 @@
 const question = [
   // {quest: "", img: false, answer: false},
-  {quest: "Какие структуры есть в Ассемблере", img: "asm_struct", answer: false},
+  {quest: "RVO", img: false, answer: "Return Value Optimization"},
   {quest: "section .txt", img: "section_txt", answer: false},
-  {quest: "section .bss const variables", img: "section_bss_const_vars", answer: false},
-  {quest: "section .bss", img: "section_bss", answer: false},
+  {quest: "CLR", img: false, answer: "Common Language Runtime(общеязыковая исполняющая среда) C# Virtual Machine"},
+  {quest: "Dalvik", img: false, answer: "..a Java Virtual Machine"},
   {quest: "section .data variables", img: "section_data_vars", answer: false},
+  {quest: "rsp register", img: false, answer: "stack pointer"},
+  {quest: "section .bss", img: "section_bss", answer: false},
+  {quest: "Compilation(translation/build) Model", img: false, answer: "Compiled(C++, C, Go, Pascal), Interpreted(JS, Java, bash, Lua), hybrid(JS, Java, Python, C#)"},
+  {quest: "movq (in assembler)", img: false, answer: "q -> quad word"},
   {quest: "section .data", img: "section_data", answer: false},
+  {quest: "Compiling stages", img: false, answer: "1. lexical analysys<br>2. syntax analysys<br>3. semantic analysys<br>4. intermedian analysys<br>5. optimization<br>6. machine code generation<br>7. linking"},
+  {quest: "Какие структуры есть в Ассемблере", img: "asm_struct", answer: false},
+  {quest: "rip register", img: false, answer: "p -> PC(program counter) общий<br>eip(instruction pointer) специфический"},
+  {quest: "section .bss const variables", img: "section_bss_const_vars", answer: false},
 ];
-
-main();
-
-function main(){
-  const question_box = document.createElement("div");
-
-  for(let i = 0; i < question.length; ++i){
-    const det = document.createElement("details");
-    const summ = document.createElement("summary");
-    const p = document.createElement("p");
-    
-    if(question[i].img != false){
-      const img = new Image;
-      img.src = "img/" + question[i].img + ".png";
-      det.appendChild(img);
-    }
-
-    summ.textContent = question[i].quest;
-    det.appendChild(summ);
-    
-    if(question[i].answer != false){
-      p.innerText = question[i].answer;
-      det.appendChild(p);
-    }
-
-    question_box.appendChild(det);
-  }
-
-  document.body.appendChild(question_box);
-} 
---
+++ main.js
@@ -0,0 +1,29 @@
+main();
+
+function main(){
+  const question_box = document.createElement("div");
+
+  for(let i = 0; i < question.length; ++i){
+    const det = document.createElement("details");
+    const summ = document.createElement("summary");
+    const p = document.createElement("p");
+    
+    if(question[i].img != false){
+      const img = new Image;
+      img.src = "img/" + question[i].img + ".png";
+      det.appendChild(img);
+    }
+
+    summ.textContent = question[i].quest;
+    det.appendChild(summ);
+    
+    if(question[i].answer != false){
+      p.innerHTML = question[i].answer;
+      det.appendChild(p);
+    }
+
+    question_box.appendChild(det);
+  }
+
+  document.body.appendChild(question_box);
+} 
