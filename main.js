55408931b3bf9b50d5109e7b5e18de26061bd4c3
khe4oyan <khe4oyan@bk.ru>
2022-06-29 21:53:24 +0400

    redesign css styles

--- main.js
+++ main.js
@@ -6,9 +6,17 @@ function main(){
   for(let i = 0; i < question.length; ++i){
     const det = document.createElement("details");
     const summ = document.createElement("summary");
-    const p = document.createElement("p");
     
     if(question[i].i != false){
+      if(Array.isArray(question[i].i)){
+        for(let k = 0; k < question[i].i.length(); ++k){
+          const img = new Image;
+          img.src = "img/" + question[i].i[k] + ".png";
+          det.appendChild(img);
+        }
+      }else{
+
+      }
       const img = new Image;
       img.src = "img/" + question[i].i + ".png";
       det.appendChild(img);
@@ -18,8 +26,17 @@ function main(){
     det.appendChild(summ);
     
     if(question[i].a != false){
-      p.innerHTML = question[i].a;
-      det.appendChild(p);
+      if(Array.isArray(question[i].a)){
+        for(let l = 0; l < question[i].a.length; ++l){
+          const p = document.createElement("p");
+          p.innerText = question[i].a[l];
+          det.appendChild(p);
+        }
+      }else{
+        const p = document.createElement("p");
+        p.innerHTML = question[i].a;
+        det.appendChild(p);
+      }
     }
 
     question_box.appendChild(det);
