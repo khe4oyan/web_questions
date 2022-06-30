49ded029c64f8155d4b25ff28a31caba5608d26a
khe4oyan <khe4oyan@bk.ru>
2022-06-30 13:57:18 +0400

    devided in files

--- main.js
+++ main.js
@@ -3,38 +3,38 @@ main();
 function main(){
   const question_box = document.createElement("div");
   question_box.classList.add("container");
-  for(let i = 0; i < question.length; ++i){
+  for(let i = 0; i < data.question.length; ++i){
     const det = document.createElement("details");
     const summ = document.createElement("summary");
     
-    if(question[i].i != false){
-      if(Array.isArray(question[i].i)){
-        for(let k = 0; k < question[i].i.length(); ++k){
+    if(data.question[i].i != false){
+      if(Array.isArray(data.question[i].i)){
+        for(let k = 0; k < data.question[i].i.length(); ++k){
           const img = new Image;
-          img.src = "img/" + question[i].i[k] + ".png";
+          img.src = "img/" + data.question[i].i[k] + ".png";
           det.appendChild(img);
         }
       }else{
 
       }
       const img = new Image;
-      img.src = "img/" + question[i].i + ".png";
+      img.src = "img/" + data.question[i].i + ".png";
       det.appendChild(img);
     }
 
-    summ.textContent = question[i].q;
+    summ.textContent = data.question[i].q;
     det.appendChild(summ);
     
-    if(question[i].a != false){
-      if(Array.isArray(question[i].a)){
-        for(let l = 0; l < question[i].a.length; ++l){
+    if(data.question[i].a != false){
+      if(Array.isArray(data.question[i].a)){
+        for(let l = 0; l < data.question[i].a.length; ++l){
           const p = document.createElement("p");
-          p.innerText = question[i].a[l];
+          p.innerText = data.question[i].a[l];
           det.appendChild(p);
         }
       }else{
         const p = document.createElement("p");
-        p.innerHTML = question[i].a;
+        p.innerHTML = data.question[i].a;
         det.appendChild(p);
       }
     }
---
+++ web/less_1/data.js
@@ -0,0 +1,5 @@
+const data = {
+  question:[
+    // {q: "", i: false, a: false},
+  ],
+}
