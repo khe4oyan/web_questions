1975ad4c0abefcc9c910658a6306a9a3fdf4e9d0
khe4oyan <khe4oyan@bk.ru>
2022-07-09 01:31:13 +0400

    add 4th lesson

---
+++ web/less_4/data.js
@@ -0,0 +1,20 @@
+const data = {
+  books: false,
+  task: ["Implement Interpretator (variables, output, arithmetic operations)"],
+  question: [
+    // {q: "", i: false, a: ""},
+    {q: "DOM", i: false, a: [
+      "Document Object Model",
+      "Древовидная структура HTML разметки"
+    ]},
+    {q: "BOM", i: false, a: [
+      "Browser Object Model",
+      "",
+      "Браузерное окружение",
+      "Древовидная структура созданная браузером"
+    ]},
+    {q: "Environment", i: false, a: "Код, написанный на JavaScript"},
+    {q: "Cross-platform", i: false, a: "Работает на других платформах"},
+    {q: "Platfor = ? + ?", i: false, a: "CPU + OS"},
+  ]  
+};
